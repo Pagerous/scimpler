f50eda52286e136e4e5b0e9734f7700941d963d0
Arek Pajor <arkadiuszpajor97@gmail.com>
2023-10-26 22:52:07 +0200

    feat: add enterprise user extension attributes

---
+++ src/parser/attributes/user_enterprise_extension.py
@@ -0,0 +1,121 @@
+from ..attributes import type as at
+from .attributes import (
+    Attribute,
+    AttributeIssuer,
+    AttributeMutability,
+    AttributeReturn,
+    AttributeUniqueness,
+    ComplexAttribute,
+)
+
+employee_number = Attribute(
+    name="employeeNumber",
+    issuer=AttributeIssuer.BOTH,
+    type_=at.String,
+    required=False,
+    case_exact=False,
+    multi_valued=False,
+    mutability=AttributeMutability.READ_WRITE,
+    returned=AttributeReturn.DEFAULT,
+    uniqueness=AttributeUniqueness.NONE,
+)
+
+cost_center = Attribute(
+    name="costCenter",
+    issuer=AttributeIssuer.BOTH,
+    type_=at.String,
+    required=False,
+    case_exact=False,
+    multi_valued=False,
+    mutability=AttributeMutability.READ_WRITE,
+    returned=AttributeReturn.DEFAULT,
+    uniqueness=AttributeUniqueness.NONE,
+)
+
+organization = Attribute(
+    name="organization",
+    issuer=AttributeIssuer.BOTH,
+    type_=at.String,
+    required=False,
+    case_exact=False,
+    multi_valued=False,
+    mutability=AttributeMutability.READ_WRITE,
+    returned=AttributeReturn.DEFAULT,
+    uniqueness=AttributeUniqueness.NONE,
+)
+
+division = Attribute(
+    name="division",
+    issuer=AttributeIssuer.BOTH,
+    type_=at.String,
+    required=False,
+    case_exact=False,
+    multi_valued=False,
+    mutability=AttributeMutability.READ_WRITE,
+    returned=AttributeReturn.DEFAULT,
+    uniqueness=AttributeUniqueness.NONE,
+)
+
+department = Attribute(
+    name="department",
+    issuer=AttributeIssuer.BOTH,
+    type_=at.String,
+    required=False,
+    case_exact=False,
+    multi_valued=False,
+    mutability=AttributeMutability.READ_WRITE,
+    returned=AttributeReturn.DEFAULT,
+    uniqueness=AttributeUniqueness.NONE,
+)
+
+
+_manager__value = Attribute(
+    name="value",
+    issuer=AttributeIssuer.BOTH,
+    type_=at.String,
+    multi_valued=False,
+    required=False,
+    case_exact=False,
+    mutability=AttributeMutability.READ_WRITE,
+    returned=AttributeReturn.DEFAULT,
+    uniqueness=AttributeUniqueness.NONE,
+)
+
+_manager__ref = Attribute(
+    name="$ref",
+    issuer=AttributeIssuer.BOTH,
+    type_=at.SCIMReference,
+    reference_types=["User"],
+    multi_valued=False,
+    required=False,
+    case_exact=False,
+    mutability=AttributeMutability.READ_WRITE,
+    returned=AttributeReturn.DEFAULT,
+    uniqueness=AttributeUniqueness.NONE,
+)
+
+_manager__display_name = Attribute(
+    name="$ref",
+    issuer=AttributeIssuer.BOTH,
+    type_=at.String,
+    multi_valued=False,
+    required=False,
+    case_exact=False,
+    mutability=AttributeMutability.READ_ONLY,
+    returned=AttributeReturn.DEFAULT,
+    uniqueness=AttributeUniqueness.NONE,
+)
+
+manager = ComplexAttribute(
+    sub_attributes=[
+        _manager__value,
+        _manager__ref,
+        _manager__display_name,
+    ],
+    name="manager",
+    issuer=AttributeIssuer.BOTH,
+    required=False,
+    multi_valued=False,
+    mutability=AttributeMutability.READ_WRITE,
+    returned=AttributeReturn.DEFAULT,
+)
