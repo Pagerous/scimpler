3d9a362752a24c179294184d633e4a18e2432016
Arek Pajor <arkadiuszpajor97@gmail.com>
2024-04-29 15:15:42 +0200

    feat: validate 'locale' User attribute

--- src/assets/schemas/user.py
+++ src/assets/schemas/user.py
@@ -153,7 +153,28 @@ preferred_language = String(
     validators=[_validate_preferred_language],
 )
 
-# TODO: write proper validator for this field according to: https://www.rfc-editor.org/rfc/rfc7231#section-5.3.5
+
+_LANGUAGE_TAG_REGEX = re.compile(
+    "^(((en-GB-oed|i-ami|i-bnn|i-default|i-enochian|i-hak|i-klingon|i-lux|"
+    "-mingo|i-navajo|i-pwn|i-tao|i-tay|i-tsu|sgn-BE-FR|sgn-BE-NL|sgn-CH-DE)|(art-lojban|"
+    "el-gaulish|no-bok|no-nyn|zh-guoyu|zh-hakka|zh-min|zh-min-nan|zh-xiang))|(("
+    "([A-Za-z]{2,3}(-([A-Za-z]{3}(-[A-Za-z]{3}){0,2}))?)|[A-Za-z]{4}|[A-Za-z]{5,8})"
+    "(-([A-Za-z]{4}))?(-([A-Za-z]{2}|[0-9]{3}))?(-([A-Za-z0-9]{5,8}"
+    "|[0-9][A-Za-z0-9]{3}))*(-([0-9A-WY-Za-wy-z](-[A-Za-z0-9]{2,8})+))*"
+    "(-(x(-[A-Za-z0-9]{1,8})+))?)|(x(-[A-Za-z0-9]{1,8})+))$"
+)
+
+
+def _validate_locale(value: str) -> ValidationIssues:
+    issues = ValidationIssues()
+    if _LANGUAGE_TAG_REGEX.fullmatch(value) is None:
+        issues.add_error(
+            issue=ValidationError.bad_value_syntax(),
+            proceed=True,
+        )
+    return issues
+
+
 locale = String(
     name="locale",
     required=False,
@@ -161,6 +182,7 @@ locale = String(
     multi_valued=False,
     mutability=AttributeMutability.READ_WRITE,
     returned=AttributeReturn.DEFAULT,
+    validators=[_validate_locale],
 )
 
 # TODO: write proper validator for this field according to: https://www.rfc-editor.org/rfc/rfc6557 ("Olson")
--- tests/assets/schemas/test_user.py
+++ tests/assets/schemas/test_user.py
@@ -16,3 +16,20 @@ def test_correct_preferred_langauge_is_validated(user_data_client):
     issues = User.validate(user_data_client)
 
     assert issues.to_dict(msg=True) == {}
+
+
+def test_bad_locale_is_validated(user_data_client):
+    user_data_client["locale"] = "pl-OT-ka"
+    expected_issues = {"locale": {"_errors": [{"code": 1}]}}
+
+    issues = User.validate(user_data_client)
+
+    assert issues.to_dict() == expected_issues
+
+
+def test_correct_locale_is_validated(user_data_client):
+    user_data_client["locale"] = "en-US"
+
+    issues = User.validate(user_data_client)
+
+    assert issues.to_dict(msg=True) == {}
