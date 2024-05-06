37735d06a9620f731c526999d47903aa1383a162
Arek Pajor <arkadiuszpajor97@gmail.com>
2024-05-06 21:54:25 +0200

    feat: add descriptions to ResourceType attributes

--- src/assets/schemas/resource_type.py
+++ src/assets/schemas/resource_type.py
@@ -20,49 +20,78 @@ class _ResourceType(ServiceResourceSchema):
             attrs=[
                 String(
                     name="id",
+                    description=(
+                        "The resource type's server unique id. "
+                        "May be the same as the 'name' attribute."
+                    ),
                     mutability=AttributeMutability.READ_ONLY,
                     issuer=AttributeIssuer.SERVER,
                 ),
                 String(
                     name="name",
+                    description=(
+                        "The resource type name.  When applicable, "
+                        "service providers MUST specify the name, e.g., 'User'."
+                    ),
                     mutability=AttributeMutability.READ_ONLY,
                     issuer=AttributeIssuer.SERVER,
                     required=True,
                 ),
                 String(
                     name="description",
+                    description=(
+                        "The resource type's human-readable "
+                        "description. When applicable, service providers MUST "
+                        "specify the description."
+                    ),
                     mutability=AttributeMutability.READ_ONLY,
                     issuer=AttributeIssuer.SERVER,
                 ),
                 URIReference(
                     name="endpoint",
+                    description=(
+                        "The resource type's HTTP-addressable "
+                        "endpoint relative to the Base URL, e.g., '/Users'."
+                    ),
                     mutability=AttributeMutability.READ_ONLY,
                     issuer=AttributeIssuer.SERVER,
                     required=True,
                 ),
                 String(
                     name="schema",
+                    description="The resource type's primary/base schema URI.",
                     mutability=AttributeMutability.READ_ONLY,
                     issuer=AttributeIssuer.SERVER,
                     required=True,
                 ),
                 Complex(
+                    name="schemaExtensions",
+                    multi_valued=True,
+                    description="A list of URIs of the resource type's schema extensions.",
                     sub_attributes=[
-                        String(
+                        URIReference(
                             name="schema",
+                            description="The URI of a schema extension.",
                             mutability=AttributeMutability.READ_ONLY,
                             issuer=AttributeIssuer.SERVER,
                             required=True,
                         ),
                         Boolean(
                             name="required",
+                            description=(
+                                "A Boolean value that specifies whether "
+                                "or not the schema extension is required for the "
+                                "resource type.If true, a resource of this type MUST "
+                                "include this schema extension and also include any "
+                                "attributes declared as required in this schema extension. "
+                                "If false, a resource of this type MAY omit this schema "
+                                "extension."
+                            ),
                             mutability=AttributeMutability.READ_ONLY,
                             issuer=AttributeIssuer.SERVER,
                             required=True,
                         ),
                     ],
-                    name="schemaExtensions",
-                    multi_valued=True,
                 ),
             ],
         )
