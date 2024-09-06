4f7b8eec47a7a6f54b06854af0725041c6564ae3
Arek Pajor <arkadiuszpajor97@gmail.com>
2024-09-06 15:38:27 +0200

    docs: provide docs for scimpler.data.scim_data

--- src/scimpler/data/scim_data.py
+++ src/scimpler/data/scim_data.py
@@ -59,36 +59,58 @@ class _BoundedAttrKey(_AttrKey):
     extension: bool
 
 
-class SCIMData(MutableMapping):
 class ScimData(MutableMapping):
+    """
+    Mapping that implements reading and updating data which is in line with SCIM requirements.
+    Enables convenient interaction, where keys are just attribute representations, and abstracts
+    all inner complexities.
+
+    According to SCIM requirements, complex attributes can not contain nested complex attributes
+    (it is general rule, but there are exceptions). It would mean the 2-level nesting should be
+    enough (or 3, for data from extensions), but `ScimData` has no such limitation. Every nesting
+    level has no awareness of its parent key.
+    """
+
     def __init__(
         self, d: Optional[Union[Mapping[str, Any], Mapping[AttrRep, Any], "ScimData"]] = None
     ):
+        """
+        Args:
+            d: Optional data to initialize `ScimData` with. Can be any mapping that has keys of
+                type of `str`, `AttrRep`, or `BoundedAttrRep`. Every key with other type is
+                ignored.
+
+        Examples:
+            >>> ScimData({"myAttr": "value", "myOtherAttr": {"mySubAttr": 42}}).to_dict()
+            {"myAttr": "value", "myOtherAttr": {"mySubAttr": 42}}
+
+            >>> ScimData(
+            >>>     {
+            >>>         AttrRep(attr="myAttr"): "value",
+            >>>         AttrRep(attr="myOtherAttr", sub_attr="mySubAttr"): 42
+            >>>     }
+            >>> ).to_dict()
+            {"myAttr": "value", "myOtherAttr": {"mySubAttr": 42}}
+
+            >>> from scimpler.schemas import UserSchema
+            >>>
+            >>> user = UserSchema()
+            >>> ScimData(
+            >>>     {user.attrs.name__formatted: "John Doe", user.attrs.externalId: "42"}
+            >>> ).to_dict()
+            {"name": {"formatted": "John Doe"}, "externalId": "42"}
+        """
         self._data: dict[str, Any] = {}
         self._lower_case_to_original: dict[str, str] = {}
 
-        if isinstance(d, SCIMData):
+        if isinstance(d, ScimData):
             self._data = d._data
             self._lower_case_to_original = d._lower_case_to_original
         elif isinstance(d, Mapping):
             for key, value in d.items():
-                if isinstance(key, AttrRep):
-                    self.set(key, value)
-
-                if not isinstance(key, str):
+                if not isinstance(key, (str, AttrRep)):
                     continue
-
-                if original_key := self._lower_case_to_original.get(key.lower()):
-                    self._data.pop(original_key, None)
-                self._lower_case_to_original[key.lower()] = key
-                if isinstance(value, Mapping):
-                    self._data[key] = SCIMData(value)
-                elif isinstance(value, list):
-                    self._data[key] = [
-                        SCIMData(item) if isinstance(item, Mapping) else item for item in value
-                    ]
-                else:
-                    self._data[key] = value
+                self.set(key, value)
 
     def __repr__(self):
         return f"{self.__class__.__name__}({str(self._data)})"
@@ -118,6 +140,82 @@ class ScimData(MutableMapping):
         key: Union[str, AttrRep, _SchemaKey, _AttrKey],
         value: Any,
     ) -> None:
+        """
+        Sets the entry in the mapping. Equivalent to `data[key] = value`. The way how the
+        value is actually set depends on the type of `key`:
+
+        - if `str` is provided, the key is parsed to one of supported key types and used as follows,
+        - if `SchemaURI` representing schema extension is provided, the `value` is set directly
+            under the key,
+        - if `SchemaURI` representing base schema is provided, a `ValueError` exception is raised,
+        - if `AttrRep` is provided, the `value` is set directly under the key, if `key` represents
+            top-level attribute, or it is nested under `AttrRep.sub_attr`, if it represents
+            a sub-attribute,
+        - if `BoundedAttrRep` is provided, the `value` is set in the same way as for `AttrRep`
+            for attributes that belong to base schemas. For attributes from extensions,
+            the value is additionally nested in schema URI key namespace.
+
+
+        Raises:
+            ValueError: If `key` is `SchemaURI` reprensenting base schema.
+            KeyError: If trying to set sub-attribute value to existing parent that is
+                not single-valued complex attribute value.
+
+        Args:
+            key: The key for which the `value` should be set.
+            value: The value to be set.
+
+        Examples:
+            >>> data = ScimData()
+            >>> data.set("userName", "johndoe")
+            >>> data.to_dict()
+            {"userName": "johndoe"}
+
+            >>> data = ScimData()
+            >>> data.set("name.formatted", "John Doe")
+            >>> data.to_dict()
+            {"name": {"formatted": "John Doe"}}
+
+            >>> data = ScimData()
+            >>> data.set(
+            >>>     "urn:ietf:params:scim:schemas:core:2.0:User:name.formatted", "John Doe"
+            >>> )
+            >>> data.to_dict()
+            {"name": {"formatted": "John Doe"}}
+
+            >>> data = ScimData()
+            >>> data.set(
+            >>>     "urn:ietf:params:scim:schemas:extension:enterprise:2.0:User",
+            >>>     {
+            >>>         "employeeNumber": "42",
+            >>>         "manager": {"displayName": "John Doe"}
+            >>>     }
+            >>> )
+            >>> data.to_dict()
+            {
+                "urn:ietf:params:scim:schemas:extension:enterprise:2.0:User": {
+                    "employeeNumber": "42"
+                    "manager": {
+                        "displayName": "John Doe"
+                    }
+                }
+            }
+
+            >>> data = ScimData()
+            >>> data.set(
+            >>>     "urn:ietf:params:scim:schemas:extension:enterprise:2.0:User"
+            >>>     ":manager.value",
+            >>>     "10",
+            >>> )
+            >>> data.to_dict()
+            {
+                "urn:ietf:params:scim:schemas:extension:enterprise:2.0:User": {
+                    "manager": {
+                        "value": "10"
+                    }
+                }
+            }
+        """
         if isinstance(value, Mapping):
             value = ScimData(value)
         elif not isinstance(value, str) and isinstance(value, Iterable):
@@ -157,16 +255,84 @@ class ScimData(MutableMapping):
             self._data[parent_attr_key] = ScimData()
 
         parent_value = self._data[self._lower_case_to_original[parent_attr_key.lower()]]
-        if not self._is_child_value_compatible(parent_value, value):
+        if not isinstance(parent_value, ScimData):
             raise KeyError(f"can not assign ({key.sub_attr}, {value}) to '{key.attr}'")
-
-        if not isinstance(value, list):
-            self._data[parent_attr_key].set(_AttrKey(attr=key.sub_attr, sub_attr=None), value)
-            return
 
         self._data[parent_attr_key].set(_AttrKey(attr=key.sub_attr, sub_attr=None), value)
 
     def get(self, key: Union[str, AttrRep, _SchemaKey, _AttrKey], default: Any = Missing) -> Any:
+        """
+        Returns the value for the specified `key`. If not found, the specified `default`
+        is returned (`Missing` object by default).
+
+        The type of `key` determines which entry is actually accessed:
+        - if `str` is provided, the key is parsed to one of supported key types and used as follows,
+        - if `SchemaURI` representing schema extension is provided, the whole subsection that
+            belongs to it is returned,
+        - if `SchemaURI` representing base schema is provided, a `ValueError` exception is raised,
+        - if `AttrRep` is provided, the retrieved value belongs to top-level attribute, or nested
+            one, kept under `AttrRep.sub_attr`, if `key` represents a sub-attribute,
+        - if `BoundedAttrRep` is provided, the value is retrieved in the same way as for `AttrRep`
+            for attributes that belong to base schemas. For attributes from extensions,
+            the value is retrieved from schema URI key namespace.
+
+        Raises:
+            ValueError: If `key` is `SchemaURI` reprensenting base schema.
+            KeyError: If trying to set sub-attribute value to existing parent that is
+                not single-valued complex attribute value.
+
+        Args:
+            key: The key for which the value should be retrieved.
+            default: The default value, if value for the `key` is not found.
+
+        Examples:
+            >>> data = ScimData(
+            >>>     {
+            >>>         "userName": "Pagerous",
+            >>>         "name": {"formatted": "AP"},
+            >>>         "emails": [
+            >>>             {"type": "work", "value": "..."}, {"type": "home", "value": "..."}
+            >>>         ],
+            >>>         "urn:ietf:params:scim:schemas:extension:enterprise:2.0:User": {
+            >>>             "employeeNumber": "42",
+            >>>             "manager": {
+            >>>                 "displayName": "John Doe"
+            >>>             }
+            >>>         }
+            >>>     }
+            >>> )
+            >>> data.get("userName")
+            "Pagerous"
+            >>> data.get("name")
+            "{"formatted": "AP"}"
+            >>> data.get("name.formatted")
+            "AP"
+            >>> data.get("urn:ietf:params:scim:schemas:core:2.0:User:name.formatted")
+            "AP"
+            >>> data.get("urn:ietf:params:scim:schemas:extension:enterprise:2.0:User")
+            {
+                "urn:ietf:params:scim:schemas:extension:enterprise:2.0:User": {
+                    "employeeNumber": "42"
+                    "manager": {
+                        "displayName": "John Doe"
+                    }
+                }
+            }
+            >>> data.get(
+            >>>     "urn:ietf:params:scim:schemas:extension:enterprise:2.0:User"
+            >>>     ":employeeNumber"
+            >>> )
+            "42"
+            >>> data.get(
+            >>>     "urn:ietf:params:scim:schemas:extension:enterprise:2.0:User"
+            >>>     ":manager.displayName"
+            >>> )
+            "John Doe"
+            >>> data.get("emails.type")
+            ["work", "home"]
+            >>> data.get("unknown")
+            Missing
+        """
         if not isinstance(key, (_SchemaKey, _AttrKey)):
             key = self._normalize(key)
 
@@ -201,6 +367,11 @@ class ScimData(MutableMapping):
         return self._data.get(attr, default)
 
     def pop(self, key: Union[str, AttrRep, _SchemaKey, _AttrKey], default: Any = Missing) -> Any:
+        """
+        Pops the `key` from the data. Works similarly to `get` with the difference that after
+        returning the value, it is not available in the data any longer.
+        """
+
         if not isinstance(key, (_SchemaKey, _AttrKey)):
             key = self._normalize(key)
 
@@ -242,7 +413,7 @@ class ScimData(MutableMapping):
             if schemas.get(value, False) is False:
                 raise KeyError(
                     f"schema {value!r} is not recognized or is an extension, so does not require "
-                    "own subspace in the data"
+                    "own namespace in the data"
                 )
             return _SchemaKey(schema=str(value))
 
@@ -253,7 +424,7 @@ class ScimData(MutableMapping):
                 if is_extension is False:
                     raise KeyError(
                         f"schema {value!r} is an extension, so does not require "
-                        "own subspace in the data"
+                        "own namespace in the data"
                     )
                 elif is_extension is True:
                     return _SchemaKey(schema=str(value))
@@ -273,22 +444,10 @@ class ScimData(MutableMapping):
             sub_attr=str(value.sub_attr) if value.is_sub_attr else None,
         )
 
-    @staticmethod
-    def _is_child_value_compatible(
-        parent_value: Any,
-        child_value: Any,
-    ) -> bool:
-        if isinstance(parent_value, list):
-            if not isinstance(child_value, list):
-                return False
-            return True
-
-        if not isinstance(parent_value, ScimData):
-            return False
-
-        return True
-
     def to_dict(self) -> dict[str, Any]:
+        """
+        Converts the `ScimData` to ordinary dictionary.
+        """
         output: dict[str, Any] = {}
         for key, value in self._data.items():
             if isinstance(value, ScimData):
